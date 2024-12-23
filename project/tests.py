d14e6e6104b20b6c8bbe13d2d77887f092139975
BlazquezMVictor <blazquezm.victor@gmail.com>
2024-12-23 10:16:55 +0100

    prueba commit desde el portatil

--- project/tests.py
+++ project/tests.py
@@ -1,19 +1,3 @@
-beginnig = "qubit"
-operation = ""
-rest_operation = ""
-stop_index = 0
+a = [1,2,3]
 
-for char in beginnig:
-    if not char.isalnum():
-        break
-
-    operation += char
-    stop_index += 1
-
-try:
-    rest_operation = beginnig[stop_index:]
-except:
-    rest_operation = ""
-
-print(operation)
-print(rest_operation)
+print(a)
